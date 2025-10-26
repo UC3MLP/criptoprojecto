33730c738b9c5dd440b89def9c4617041bf3c84b
julio <chispa.jc8@gmail.com>
2025-10-26 05:45:52 +0100

    cambio de implementación a customtk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import tkinter as tk
-from tkinter import ttk
+import customtkinter as ctk
 from tkinter import messagebox
 from db_utils import db_init
 from auth_server import AuthServer, register_user, login_user
@@ -9,76 +9,92 @@ from crypto_client import ClientCrypto
 
 # Inicializacion de tkinter
 
-class App(tk.Tk):
+class App(ctk.CTk):
     def __init__(self,AuthServer,BallotBox,bb_pub_pem):
         super().__init__()
         self.auth_server = AuthServer # Guardamos la instancia del servidor de auteticación
         self.title("sistema de votación-Acceso")
-        self.geometry("400x350")
+        self.geometry("500x500")
         self.ballot_box = BallotBox
         self.bb_pub_pem = bb_pub_pem
-        self.configure(background="#f0f0f0")
-
-    
-         #estilo para los widgets
-        style = ttk.Style(self)
-        style.theme_use('clam')
-        style.configure('TNotebook.Tab',font =('Arial',10,'bold'))
-        style.configure('TButton',font=('Arial',10),padding = 6)
-
-
-        #contenedor de pestañas(Notebook)
-        self.notebook = ttk.Notebook(self)
-        self.notebook.pack(pady=10, padx=10, expand=True, fill ="both")
-
-        #Frames para pestañas 
-        self.login_frame = ttk.Frame(self.notebook,padding ="10 10 10 10 ")
-        self.register_frame = ttk.Frame(self.notebook,padding = "10 10 10 10 ")
-        self.notebook.add(self.login_frame,text= "Iniciar Sesión")
-        self.notebook.add(self.register_frame,text= "Registrarse")
         
-        #etiqueta de estado para mensajes del sistema
-        self.status_label =ttk.Label(self,text="", font=('Arial',10,'bold'))
+
+        #Crear tabView paera las pestañas
+        self.tabview = ctk.CTkTabview(self, width =450,height = 350)
+        self.tabview.pack(pady= 20, padx=20, fill ="both", expand =True)
+
+        #crear pestañas
+        self.tabview.add("Iniciar Sesión")
+        self.tabview.add("Registrarse")
+        self.status_label =ctk.CTkLabel(self,text="", font=('Arial',10,'bold'))
         self.status_label.pack(pady= 5)
 
-        #Creando las interfaces
-        self.create_login_widgets()
+        #Etiqueta de estado
+        self.status_label=ctk.CTkLabel(
+            self,
+            text= "",
+            font = ("Arial",12,"bold"),
+            text_color ="blue"
+        )
+        self.status_label.pack(pady=10)
+        
+        #Interfaces
+        self.create_login_widgets(
+        )
         self.create_register_widgets()
 
-    #widgets de inicio de sesion 
     def create_login_widgets(self):
-        #etiquetas y campos de entrada
+        login_frame=self.tabview.tab("Iniciar Sesión")
+
+
+        #mail
+        ctk.CTkLabel(
+            login_frame,
+            text ="Email:",
+            font =("Arial",13)).grid(row =0, column=0, sticky ='w',pady =10,padx=20)
         
-        ttk.Label(self.login_frame, text ="Email:", font=('Arial',10)).grid(row=0,column =0,sticky='w', pady=5)
-        self.login_email_entry = ttk.Entry(self.login_frame,width=30)
-        self.login_email_entry.grid(row=0,column=1,pady=5,padx=5)
+        self.login_email_entry = ctk.CTkEntry(login_frame,width=250, placeholder_text="correo_ejemplo@gmail.com")
 
-        ttk.Label(self.login_frame, text ="Contraseña:", font=('Arial',10)).grid(row=2,column =0,sticky='w', pady=5)
-        self.login_pwd_entry = ttk.Entry(self.login_frame,width=30)
-        self.login_pwd_entry.grid(row=2,column=1,pady=5,padx=5)
+        self.login_email_entry.grid(row=0,column=1,pady=10,padx=10)
 
+        #Contraseña
+        ctk.CTkLabel(
+            login_frame,
+            text ="Contraseña:",
+            font =("Arial",13)).grid(row =1, column=0, sticky ='w',pady =10,padx=20)
+        
+        self.login_pwd_entry = ctk.CTkEntry(login_frame,width=250, placeholder_text="Contraseña")
+
+        self.login_pwd_entry.grid(row=1,column=1,pady=10,padx=10) 
 
         #Boton para el Login
-        ttk.Button(self.login_frame, text = "Iniciar sesión", command =self.handle_login).grid(row =3,column=0, columnspan=2, pady=20)
+        ctk.CTkButton(login_frame, text = "Iniciar sesión", command =self.handle_login,
+                      width=200, height=35, font=("Arial",13,"bold")).grid(row =2,column=0, columnspan=2, pady=30)
 
     def create_register_widgets(self):
-
+        register_frame = self.tabview.tab("Registrarse")
+        
         #etiquetas y campos de entrada
-        ttk.Label(self.register_frame, text ="DNI:", font=('Arial',10)).grid(row=0,column =0,sticky='w', pady=5)
-        self.reg_dni_entry = ttk.Entry(self.register_frame,width=30)
+        #DNI
+        ctk.CTkLabel(register_frame, text ="DNI:", font=('Arial',13)).grid(row=0,column =0,sticky='w', pady=10, padx=20)
+        self.reg_dni_entry = ctk.CTkEntry(register_frame,width=250, placeholder_text="12345677A")
         self.reg_dni_entry.grid(row=0,column=1,pady=5,padx=5)
 
+        #Email
+        ctk.CTkLabel(register_frame, text ="Email:", font=('Arial',13)).grid(row=1,column =0,sticky='w', pady=10, padx=20)
+        self.reg_email_entry = ctk.CTkEntry(register_frame,width=250, placeholder_text="corrreo@gmail.com")
+        self.reg_email_entry.grid(row=1,column=1,pady=10,padx=10)
 
-        ttk.Label(self.register_frame, text ="Email:", font=('Arial',10)).grid(row=1,column =0,sticky='w', pady=5)
-        self.reg_email_entry = ttk.Entry(self.register_frame,width=30)
-        self.reg_email_entry.grid(row=1,column=1,pady=5,padx=5)
+   
+        #contraseña
+        ctk.CTkLabel(register_frame, text ="Contraseña:", font=('Arial',13)).grid(row=2,column =0,sticky='w', pady=10, padx=20)
+        self.reg_pwd_entry = ctk.CTkEntry(register_frame,width=250, show="*",placeholder_text="contraseña")
+        self.reg_pwd_entry.grid(row=2,column=1,pady=10,padx=10)
 
-        ttk.Label(self.register_frame, text ="Contraseña:", font=('Arial',10)).grid(row=2,column =0,sticky='w', pady=5)
-        self.reg_pwd_entry = ttk.Entry(self.register_frame, show="*", width=30)
-        self.reg_pwd_entry.grid(row=2,column=1,pady=5,padx=5)
 
          #Boton para el Registro
-        ttk.Button(self.register_frame, text = "Registrarse ", command =self.handle_register).grid(row =3,column=0, columnspan=2, pady=20)
+        ctk.CTkButton(register_frame, text = "Registrarse ", command =self.handle_register,width= 200, height=35,
+                      font=("Arial",13,"bold")).grid(row =3,column=0, columnspan=2, pady=30)
 
 
     #lógica para el register
@@ -88,26 +104,26 @@ class App(tk.Tk):
         password = self.reg_pwd_entry.get()
 
         #Limpiar el mensaje anterior si existe
-        self.status_label.config(text="")
+        self.status_label.configure(text="")
 
         try:
             register_user(email,dni,password)
-            self.status_label.config(text="Registro completado, debes iniciar sesión",foreground="blue")
+            self.status_label.configure(text="Registro completado, debes iniciar sesión",text_color="green")
             # Limpiar campos despues del registro
             self.reg_dni_entry.delete(0,tk.END)
             self.reg_email_entry.delete(0,tk.END)
             self.reg_pwd_entry.delete(0,tk.END)
             #Cambiar a la pestaña de login
-            self.notebook.select(self.login_frame)
+            self.tabview.set("Iniciar Sesión")
 
         except ValueError as e:
-            self.status_label.config(text=f"Error de validadción:{e}", foreground="red")
+            self.status_label.configure(text=f"Error de validadción:{e}", text_color="red")
         except Exception as e:
             # Error de base de datos en caso de que existan por ejemplo ya el dni o el email
             if "UNIQUE constrait failed" in str(e):
-                self.status_label.config(text="Error Registro DNI o Email ya está registrado", foreground="red")
+                self.status_label.configure(text="Error Registro DNI o Email ya está registrado", text_color="red")
             else:
-                self.status_label.config(text=f"Error :{e}", foreground="red")
+                self.status_label.configure(text=f"Error :{e}", text_color="red")
 
     
     #Lógica para el login
@@ -123,7 +139,7 @@ class App(tk.Tk):
                 #tras esto se llama a la siguiente venxtana donde se realiza la votación
                 self.show_voting_interface(dni_user)
         except ValueError as e:
-            self.status_label.config(text =str(e),foreground='red')
+            self.status_label.configure(text =str(e),text_color='red')
 
         
   #  Interfaz de votación
@@ -146,13 +162,12 @@ class App(tk.Tk):
             self.deiconify()
 
 # Interfaz para la votación
-class VotingInterface(tk.Toplevel):
+class VotingInterface(ctk.CTkToplevel):
     def __init__(self, master,dni,auth_server, ballot_box, bb_pub_pem):
         #tk.Toplevel crea una ventana secundaria
         super().__init__(master)
         self.title("Plataforma de votación")
-        self.geometry("400x300")
-        self.configure(background="#f0f0f0")
+        self.geometry("550x400")
 
         #Datos y módulos clave
         self.dni = dni
@@ -166,59 +181,74 @@ class VotingInterface(tk.Toplevel):
         self.eligibility_token = None
 
         #título de la ley
-        self.law_title_label= ttk.Label(self,text =f"Ley a votar:{self.election_id}",
-                  font=('Arial',12,'bold'))
+        self.law_title_label= ctk.CTkLabel(self,text =f"Ley a votar:{self.election_id}",
+                  font=('Arial',16,'bold'))
         self.law_title_label.pack(pady=10)
         
 
         #Boton para cambiar de ley
-        ttk.Button(self, text = 'Cambiar ley',command=self.show_election_selector).pack(pady=5)
+        ctk.CTkButton(self, text = 'Cambiar ley',command=self.show_election_selector, width =150,
+                      height=30).pack(pady=10)
 
         #Etiqueta de estado
-        self.status_label = ttk.Label(self,text="obteniendo token",foreground="blue")
-        self.status_label.pack(pady=5)
+        self.status_label = ctk.CTkLabel(self,text="obteniendo token",font=("Arial",12),text_color="white")
+        self.status_label.pack(pady=10)
+
+
+
         self.crypto_client = ClientCrypto(bb_pub_pem)
 
+
+
+
+
         #Frame para los botones de votación
-        button_frame = ttk.Frame(self)
-        button_frame.pack(pady=20)
+        button_frame = ctk.CTkFrame(self,fg_color="transparent")
+        button_frame.pack(pady=30)
 
         #Botones de vot
-        ttk.Button(button_frame, text='✅ Voto a favor', command= lambda: self.handle_vote("SI")).pack(side=tk.LEFT, padx= 15)
-        ttk.Button(button_frame, text='❌ Voto en contra', command= lambda: self.handle_vote("NO")).pack(side=tk.LEFT, padx= 15)
-        ttk.Button(button_frame, text='⚪️ Abstención', command= lambda: self.handle_vote("ABSTENCIÓN")).pack(side=tk.LEFT, padx= 15)
+        ctk.CTkButton(button_frame, text='✅ Voto a favor', command= lambda: self.handle_vote("SI"), width=150, height=40,fg_color="green",
+                   hover_color="darkgreen",font=("Arial",12,"bold")).pack(side=tk.LEFT, padx= 10)
+        ctk.CTkButton(button_frame, text='❌ Voto en contra', command= lambda: self.handle_vote("NO"), width=150, height=40,fg_color="red",
+                   hover_color="darkred",font=("Arial",12,"bold")).pack(side=tk.LEFT, padx= 10)
+        ctk.CTkButton(button_frame, text='⚪️ Abstención', command= lambda: self.handle_vote("ABSTENCIÓN"), width=150, height=40,fg_color="gray",
+                   hover_color="darkgray",font=("Arial",12,"bold")).pack(side=tk.LEFT, padx= 10)
 
        
 
     def show_election_selector(self):
         """ Muestra una ventana para elegir una nueva ley y reiniciar la votación
         """
-        laws= {"Ley 1": "Propuesta de Ley 1",
+        laws= {"Ley 1": "Ley 1",
                "Ley 2":"Propuesta de Ley 2",
                "Ley 3": "Propuesta de Ley 3"
                }
         
-        #Configuración de la ventana de selección
-        selector_window = tk.Toplevel(self)
+        #configureuración de la ventana de selección
+        selector_window = ctk.CTkToplevel(self)
         selector_window.title("Seleccionar Ley")
+        selector_window.geometry("350x300")
         selector_window.transient(self)#hace que la ventana este arriba siempre
 
-        ttk.Label(selector_window, text = "Elige la Ley para la que quieres votas").pack(padx=20,pady=10)
+        ctk.CTkLabel(selector_window, text = "Elige la Ley para la que quieres votar",
+                     font=("Arial",13,"bold")).pack(padx=20,pady=20)
 
             #Gestor de la selección
         def select_law(law_id, law_name):
             #Cierra la ventana de selección anterior
             selector_window.destroy()
             self.election_id = law_id
-            self.law_title_label.config(text= f"Ley a votar: {self.election_id}")
-            self.status_label.config(text = f"Cambio a :{law_name}",foreground= "blue")
+            self.law_title_label.configure(text= f"Ley a votar: {self.election_id}")
+            self.status_label.configure(text = f"Cambio a :{law_name}",text_color= "white")
 
             #Limpia el token anterior y reinicia el proceso de elegibilidad
             self.eligibility_token = None
             #Botones para cada ley
         for law_name, law_id in laws.items():
-            ttk.Button(selector_window,text = law_name,
-                    command=lambda l_id=law_id,l_name=law_name:select_law(l_id,l_name)).pack(pady =5, padx=20)
+            ctk.CTkButton(selector_window,text = law_name,
+                    command=lambda l_id=law_id,
+                    l_name=law_name:select_law(l_id,l_name),
+                    width=250, height=35).pack(pady =10, padx=20)
 
     
     def get_eligibility_token(self):
@@ -233,18 +263,18 @@ class VotingInterface(tk.Toplevel):
             self.eligibility_token = token
 
             #Actualizar a la ley actual
-            self.law_title_label.config (text= f"Ley Actual: {self.election_id}")
-            self.status_label.config(text= f"Token obtenido. DNI{self.dni [-4:]}...", foreground = "green")
+            self.law_title_label.configure (text= f"Ley Actual: {self.election_id}")
+            self.status_label.configure(text= f"Token obtenido. DNI{self.dni [-4:]}...", text_color = "green")
             return True
 
         except ValueError as e:
-            self.status_label.config(text = f"ERROR: {e}", foreground= "red")
+            self.status_label.configure(text = f"ERROR: {e}", text_color= "red")
             messagebox.showerror("Error de Voto", str(e))
             return False
             
         #Para cualquier otro error
         except Exception as e:
-            self.status_label.config(text = f"ERROR AS : {e}", foreground= "red")
+            self.status_label.configure(text = f"ERROR AS : {e}", text_color= "red")
             messagebox.showerror("Error Crítico ",f"No se pudo obtener el token: {e}" )
             return False
   
